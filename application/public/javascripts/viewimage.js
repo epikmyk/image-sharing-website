1278bcc7bf213bfea9dc1de42f813f564de399ee
epikmyk <mgmorales1989@gmail.com>
2020-05-17 16:41:31 -0700

    allow user to comment only when logged in

---
+++ application/public/javascripts/viewimage.js
@@ -0,0 +1,64 @@
+
+function logoutClick(event) {
+    var fetchOptions = {
+        method: "POST",
+        body: '',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    let fetchURL = "http://localhost:3000/users/logout";
+    fetch(fetchURL, fetchOptions)
+        .then((data) => {
+            console.log(data);
+            let logButton = document.getElementById('login');
+            logButton.innerHTML = "LOGIN";
+            logButton.setAttribute('href', '/login');
+            logButton.onclick = null;
+
+            let commentLogButton = document.getElementById('comment-login');
+            let commentButton = document.createElement("a");
+            commentButton.setAttribute("href", "/login");
+
+            commentButton.id = "comment-button";
+            commentButton.parentNode.replaceChild(commentLogButton, commentButton);
+
+
+            let commentPlaceholder = document.getElementById('leave-comment');
+            let commentTextBox = document.createElement("p");
+            commentTextBox.id = 'leave-comment';
+            commentTextBox.name = 'leave-comment';
+            commentPlaceholder.parentNode.replaceChild(commentPlaceholder, commentTextBox);
+        }).catch((err) => location.reload());
+
+}
+
+if (document.cookie.includes('csid')) {
+    console.log('user is logged in');
+    let logButton = document.getElementById('login');
+    logButton.innerHTML = "LOGOUT";
+    logButton.removeAttribute('href');
+    logButton.onclick = logoutClick;
+
+
+    let commentLogButton = document.getElementById('comment-login');
+    let commentButton = document.createElement("INPUT");
+    commentButton.setAttribute("type", "submit");
+    commentButton.value = "Comment";
+    commentButton.id = "comment-button";
+    commentLogButton.parentNode.replaceChild(commentButton, commentLogButton);
+
+    let commentPlaceholder = document.getElementById('leave-comment');
+    let commentTextBox = document.createElement("INPUT");
+    commentTextBox.setAttribute("type", "text");
+    commentTextBox.id = 'comments';
+    commentTextBox.name = 'comments';
+    commentPlaceholder.parentNode.replaceChild(commentTextBox, commentPlaceholder);
+
+}
+else {
+
+    let logButton = document.getElementById('login');
+    logButton.innerHTML = "LOGIN";
+    logButton.setAttribute('href', '/login');
+}
